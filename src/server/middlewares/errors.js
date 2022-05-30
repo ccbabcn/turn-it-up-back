370f74c896daf85f73a2ee6d5b0f1446bddd53c1
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-30 21:27:22 +0200

    implemented utils custom error and its test

---
+++ src/server/middlewares/errors.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const customError = require("../../utils/customError/customError");
+
+const notFoundError = (req, res, next) => {
+  const error = customError(404, "Endpoint not found");
+
+  next(error);
+};
+
+module.exports = notFoundError;
