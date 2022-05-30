1d6874bd0db28160f810f32ee5faa3e8940b8091
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-30 19:51:49 +0200

    added corsOptions for security problems on sonarcloud

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const corsOptions = require("../utils/corsOptions");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
