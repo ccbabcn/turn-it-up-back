5b4640b30cdbae0b2272dae094bb83028fc5af27
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-31 10:32:56 +0200

    implemented existing user error path on userRegister

--- src/server/controllers/userControllers/userControllers.test.js
+++ src/server/controllers/userControllers/userControllers.test.js
@@ -11,6 +11,8 @@ jest.mock("bcrypt", () => ({
   hash: jest.fn().mockResolvedValue(() => "mockPasswordEncrypted"),
 }));
 
+const next = jest.fn();
+
 describe("Given a userRegister function", () => {
   describe("When it's called with a new name, username and password", () => {
     test("Then it should call the response method status with 201 and it's json method with the message 'User created'", async () => {
@@ -29,6 +31,20 @@ describe("Given a userRegister function", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
       expect(res.json).toHaveBeenCalledWith(expectedMessage);
+    });
+  });
+
+  describe("When it's called with an existing username", () => {
+    test("Then it should call next with an error", async () => {
+      const expectedError = new Error();
+      const req = {
+        body: mockUser,
+      };
+      User.findOne = jest.fn().mockResolvedValue(true);
+
+      await userRegister(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
 });
