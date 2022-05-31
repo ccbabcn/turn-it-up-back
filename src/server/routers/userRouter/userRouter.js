71a14b34bd47c16b736dfbebb14d1ea46cdf5575
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-31 11:04:15 +0200

    implemented initial userRouter register path structure

---
+++ src/server/routers/userRouter/userRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { validate } = require("express-validation");
+const userRegister = require("../../controllers/userControllers/userControllers");
+const userRegisterCredentials = require("../../schemas/userCredentials");
+
+const userRouter = express.Router();
+
+userRouter.post("/register", validate(userRegisterCredentials), userRegister);
